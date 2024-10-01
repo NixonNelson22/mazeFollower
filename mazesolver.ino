0fe5c5170289df0bfd4ba5022247429b414ca333
Nixon Nelson Paliakkara <40900101+NixonNelson22@users.noreply.github.com>
2024-10-01 17:13:36 +0530

    first commit

---
+++ mazesolver.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+// mobility script
+//// controlled by localization and path algo
+
+// mapping 
+//// get encoder data and approximate the values to a 2d matrix
+//// landmarks are +,L,O,# sections
+//// straight | sections
+//// goal is # square 
+//// has to be stored in memory
+
+// localization 
+//// get data from mapping and check if we are in correct place
+//// path error correction
+
+// shortest path algo
+//// find the shortest path from mapping
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //encoder 
+  //mapping 
+}
